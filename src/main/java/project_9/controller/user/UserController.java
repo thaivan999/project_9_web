95bdb4c32f3e9472689c1547518cb593c5feb071
ThaiVanHandSome <danitbadao1234@gmail.com>
2023-11-13 13:52:22 +0700

    init

---
+++ src/main/java/project_9/controller/user/UserController.java
@@ -0,0 +1,9 @@
+package project_9.controller.user;
+
+import javax.servlet.http.HttpServlet;
+
+public class UserController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+}
