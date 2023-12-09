3f82458552322f87603b998f86d1988f1ff1c85b
KhaIniesta <le.minh.kha.student.hcmute@gmail.com>
2023-12-09 19:13:54 +0700

    [kha] fix search with accent and unaccent, all done

--- src/main/java/hcmute/repository/MilkTeaRepository.java
+++ src/main/java/hcmute/repository/MilkTeaRepository.java
@@ -35,32 +35,39 @@ public interface MilkTeaRepository extends JpaRepository<MilkTeaEntity, Integer>
 
 	long count();
 
-	Page<MilkTeaEntity> findBynameContaining(String name, Pageable pageable);
-
-	@Query(value = "SELECT * FROM milk_tea WHERE LOWER(cast(name as varchar(1000)) collate SQL_Latin1_General_Cp1251_CS_AS) LIKE LOWER(CONCAT('%', cast(:name as varchar(1000)) collate SQL_Latin1_General_Cp1251_CS_AS, '%'))", nativeQuery = true)
+	@Query(value = "SELECT * " + "FROM milk_tea " + "WHERE "
+			+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE Vietnamese_CI_AI) OR "
+			+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE SQL_Latin1_General_CP1253_CI_AI)", nativeQuery = true)
 	List<MilkTeaEntity> findByNameContaining(@Param("name") String name);
 
-	@Query(value = "SELECT * FROM milk_tea WHERE LOWER(cast(name as varchar(1000)) collate SQL_Latin1_General_Cp1251_CS_AS) LIKE LOWER(CONCAT('%', cast(:name as varchar(1000)) collate SQL_Latin1_General_Cp1251_CS_AS, '%')) ORDER BY cost ASC", nativeQuery = true)
+	@Query(value = "SELECT * " + "FROM milk_tea " + "WHERE "
+			+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE Vietnamese_CI_AI) OR "
+			+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE SQL_Latin1_General_CP1253_CI_AI) "
+			+ "ORDER BY cost ASC", nativeQuery = true)
+	List<MilkTeaEntity> findByNameContainingAndSortAscendingByCost(@Param("name") String name);
+
+	@Query(value = "SELECT * " + "FROM milk_tea " + "WHERE "
+			+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE Vietnamese_CI_AI) OR "
+			+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE SQL_Latin1_General_CP1253_CI_AI) "
+			+ "ORDER BY cost DESC", nativeQuery = true)
+	List<MilkTeaEntity> findByNameContainingAndSortDescendingByCost(@Param("name") String name);
+
+	@Query(value = "SELECT * " + "FROM milk_tea " + "WHERE "
+			+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE Vietnamese_CI_AI) OR "
+			+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE SQL_Latin1_General_CP1253_CI_AI)", nativeQuery = true)
+	Page<MilkTeaEntity> findBynameContaining(@Param("name") String name, Pageable pageable);
+	
+	@Query(value = "SELECT * " + "FROM milk_tea " + "WHERE "
+			+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE Vietnamese_CI_AI) OR "
+			+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE SQL_Latin1_General_CP1253_CI_AI) "
+			+ "ORDER BY cost ASC", nativeQuery = true)
 	Page<MilkTeaEntity> findByNameContainingAndSortAscendingByCost(@Param("name") String name, Pageable pageable);
 
-	@Query(value = "SELECT * FROM milk_tea WHERE LOWER(cast(name as varchar(1000)) collate SQL_Latin1_General_Cp1251_CS_AS) LIKE LOWER(CONCAT('%', cast(:name as varchar(1000)) collate SQL_Latin1_General_Cp1251_CS_AS, '%')) ORDER BY cost DESC", nativeQuery = true)
+	@Query(value = "SELECT * " + "FROM milk_tea " + "WHERE "
+			+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE Vietnamese_CI_AI) OR "
+			+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE SQL_Latin1_General_CP1253_CI_AI) "
+			+ "ORDER BY cost DESC", nativeQuery = true)
 	Page<MilkTeaEntity> findByNameContainingAndSortDescendingByCost(@Param("name") String name, Pageable pageable);
-	
-	@Query(value = "SELECT * "
-				+ "FROM milk_tea "
-				+ "WHERE "
-				+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE Vietnamese_CI_AI) OR "
-				+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE SQL_Latin1_General_CP1253_CI_AI) "
-				+ "ORDER BY cost ASC", nativeQuery = true)
-	List<MilkTeaEntity> findByNameContainingAndSortAscendingByCost(@Param("name") String name);
-	
-	@Query(value = "SELECT * "
-				+ "FROM milk_tea "
-				+ "WHERE "
-				+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE Vietnamese_CI_AI) OR "
-				+ "(LOWER(name) LIKE CONCAT('%', :name, '%') COLLATE SQL_Latin1_General_CP1253_CI_AI) "
-				+ "ORDER BY cost DESC", nativeQuery = true)
-	List<MilkTeaEntity> findByNameContainingAndSortDescendingByCost(@Param("name") String name);
 
 	@Query("SELECT COUNT(mt) FROM MilkTeaEntity mt WHERE mt.milkTeaTypeByMilkTea.idType = :typeId")
 	int countByTypeId(int typeId);
@@ -71,8 +78,7 @@ public interface MilkTeaRepository extends JpaRepository<MilkTeaEntity, Integer>
 	@Query("SELECT mt FROM MilkTeaEntity mt WHERE mt.milkTeaTypeByMilkTea.idType = :idType")
 	Page<MilkTeaEntity> findAllByTypeId(int idType, Pageable pageable);
 
-	Page<MilkTeaEntity> findByNameContaining(String name, Pageable pageable);
-	
 	@Query("SELECT COALESCE(m.remainQuantity, 0) FROM MilkTeaEntity m WHERE m.idMilkTea = :idMilkTea AND m.branchByMilkTea.idBranch = :idBranch")
-	Optional<Integer> findRemainQuantityByIdMilkTeaAndIdBranch(@Param("idMilkTea") int idMilkTea, @Param("idBranch") int idBranch);
+	Optional<Integer> findRemainQuantityByIdMilkTeaAndIdBranch(@Param("idMilkTea") int idMilkTea,
+			@Param("idBranch") int idBranch);
 }
